040d229d879c14adf19f7feefd88aeef127a6cb1
Feyzanur Keleş <fyzakeles@gmail.com>
2021-11-19 02:09:08 +0300

    Word count challenge added

---
+++ WordCount/README.md
@@ -0,0 +1,3 @@
+### Word Count
+
+![questionImg](https://cdn.discordapp.com/attachments/903251813053108254/910921558732730428/Coderbyte_8.png)
---
+++ WordCount/js/wordcount.js
@@ -0,0 +1,10 @@
+function WordCount(str) { 
+  
+    // code goes here  
+    return str.split(' ').length; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(WordCount(readline()));
+  
